ee07d290fae37b8bbe845daf9f297b1e50b225f3
shanjana91 <shanjanam.91@gmail.com>
2021-05-19 15:49:47 +0530

    pom file updated

--- src/main/java/com/example/ConsumingProductApiApplication.java
+++ src/main/java/com/example/ConsumingProductApiApplication.java
@@ -1,30 +1,27 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 
 import com.example.UserRepository.UserRepository;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-
-import org.slf4j.*;
-
 
 @SpringBootApplication
 @EnableEncryptableProperties
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 
-public class ConsumingProductApiApplication implements CommandLineRunner{
-	private static Logger log=LoggerFactory.getLogger(ConsumingProductApiApplication.class);
+public class ConsumingProductApiApplication extends SpringBootServletInitializer implements CommandLineRunner {
+	private static Logger log = LoggerFactory.getLogger(ConsumingProductApiApplication.class);
+	
+	//JAR
 	public static void main(String[] args) {
-		
-		
+
 		SpringApplication.run(ConsumingProductApiApplication.class, args);
 	}
 
@@ -34,4 +31,10 @@ public class ConsumingProductApiApplication implements CommandLineRunner{
 		log.info("Application started...");
 	}
 	
+	//WAR
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(ConsumingProductApiApplication.class);
+	    }
+
 }
